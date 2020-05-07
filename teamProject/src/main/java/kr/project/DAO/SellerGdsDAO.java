b031a2355c98cf2c57f7c349940c6c4cf0358100
CHOYEJI <dbdusdk25@naver.com>
2020-05-07 15:43:01 +0900

    물품 등록 기능 구현 완료

--- teamProject/src/main/java/kr/project/DAO/SellerGdsDAO.java
+++ teamProject/src/main/java/kr/project/DAO/SellerGdsDAO.java
@@ -1,11 +1,9 @@
 package kr.project.DAO;
 
-import java.util.Map;
-
-import org.springframework.stereotype.Repository;
-
 import kr.project.VO.SellerGdsVO;
 
 public interface SellerGdsDAO {
+
+	void insert(SellerGdsVO sellerGdsVO);
 
 }
