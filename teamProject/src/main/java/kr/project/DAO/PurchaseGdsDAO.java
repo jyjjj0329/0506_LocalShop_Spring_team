dcb968ea963db0806c52beab2c9c87c7f51bea49
CHOYEJI <dbdusdk25@naver.com>
2020-06-05 15:35:30 +0900

    결제 기능 구현 완료

--- teamProject/src/main/java/kr/project/DAO/PurchaseGdsDAO.java
+++ teamProject/src/main/java/kr/project/DAO/PurchaseGdsDAO.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import kr.project.VO.BuyerVO;
 import kr.project.VO.PurchaseGdsVO;
+import kr.project.VO.SellerGdsVO;
 
 public interface PurchaseGdsDAO {
 
@@ -13,6 +14,15 @@ public interface PurchaseGdsDAO {
 	ArrayList<PurchaseGdsVO> selectList(HashMap<String, Object> hmap);
 
 	BuyerVO getbuyerid(String id);
+
+	void paymentInsert(PurchaseGdsVO purchaseGdsVO);
+
+	SellerGdsVO select(int sellerGdsVO_idx);
+
+	void paymentupdate(SellerGdsVO sellerGdsVO);
+
+	void moneyUpdate(HashMap<String, Object> hmap);
+
 
 
 }
